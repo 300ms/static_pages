19dff8aa7bc83ebd8a3a39ec6481f0ab56fda973
300ms <ilhansnmz44@gmail.com>
2019-12-19 20:27:26 +0300

    secure password feature has been added now

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  before_save { self.email = self.email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
+  validates :password,  presence: true, length: { within: 6..15 }
+end
