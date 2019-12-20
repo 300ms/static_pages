832a83283bd6b7875a556f467dc4c3877693fc0e
300ms <ilhansnmz44@gmail.com>
2019-12-20 18:25:20 +0300

    Implement basic login and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: {case_sensitive: false}, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
   has_secure_password
   validates :password,  presence: true, length: { within: 6..15 }
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
