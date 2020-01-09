48fb41eb39ab7dc26d93f2cc0832786bb77d58a4
300ms <ilhansnmz44@gmail.com>
2020-01-10 01:51:17 +0300

    Sendgrid credential problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,8 +72,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app155695705@heroku.com'],  ###  CHANGE ACCOUNT INFORMATION  ###
-    :password       => ENV['trp3g10s9933'],  ###  heroku config:get SENDGRID_USERNAME  ###
+    :user_name      => ENV['xxx'],  ###  CHANGE ACCOUNT INFORMATION  ###
+    :password       => ENV['xxx'],  ###  heroku config:get SENDGRID_USERNAME  ###
                                                   ###  heroku config:get SENDGRID_PASSWORD  ###
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
